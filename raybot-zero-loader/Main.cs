f98e461891113cc2a0ff2f4ef9422496be4588b0
Emlin <49819627+R4YVEN@users.noreply.github.com>
2022-12-22 11:21:00 +0100

    Update Main.cs

--- raybot-zero-loader/Main.cs
+++ raybot-zero-loader/Main.cs
@@ -21,13 +21,11 @@ namespace raybot_zero_loader
 
         protected override void WndProc(ref Message m)
         {
-            switch (m.Msg)
-            {
-                case 0x84:
-                    base.WndProc(ref m);
-                    if ((int)m.Result == 0x1)
-                        m.Result = (IntPtr)0x2;
-                    return;
+            if (m.Msg == 0x84) {
+                base.WndProc(ref m);
+                if ((int)m.Result == 0x1)
+                    m.Result = (IntPtr)0x2;
+                return;
             }
 
             base.WndProc(ref m);
