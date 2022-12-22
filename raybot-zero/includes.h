9576cbd37df3c77b8064cd6689249f45b9f659b1
Emlin <49819627+R4YVEN@users.noreply.github.com>
2022-12-22 11:35:22 +0100

    explained a bit more

--- raybot-zero/includes.h
+++ raybot-zero/includes.h
@@ -2,13 +2,12 @@
 
 #define PRNT(...) DbgPrintEx( DPFLTR_SYSTEM_ID, DPFLTR_ERROR_LEVEL, "[raybot-zero] " __VA_ARGS__);
 
-//custom types
+//custom types to give the code some uniform
 typedef unsigned char b8;
 typedef int i32;
 typedef unsigned int u32;
 typedef float f32;
 typedef unsigned __int64 u64;
-
 
 //kernelmode floating point fix
 extern "C" {
@@ -20,7 +19,8 @@ extern "C" {
 #include "utils.h"
 #include "memory.h"
 #include "mini_sdk.h"
-//globals
+
+//all the global variables.
 namespace _
 {
 	static bool bStopThread = false;
@@ -32,12 +32,14 @@ namespace _
 
 //for anyone wondering: a cycle means a loop in the main thread. 
 //g_Cycle just contains info about the current "mainthread-while-loop-tick" (ex: localPlayer etc).
-//could also be referred to as a tick but i thought it might get confused
-//with the actual ingame tick
+//could also be referred to as a tick but i thought it might get confused with the actual ingame tick
+//---
+//TL;DR just put everything here that might change (round-end, switching servers etc)
 namespace g_Cycle
 {
 	static entity_t* localPlayer;
 }
 
+//yes. you wanna complain? thought so.
 #include "keyinput.h"
 #include "features.h"
